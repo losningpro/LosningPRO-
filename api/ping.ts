c7adf920f5614d07d8b915cf2668f955585930c4
Daniel Danielsen <danieldanielsen.gi@gmail.com>
2026-04-05 21:58:09 +0200

    Update ping.ts

--- api/ping.ts
+++ api/ping.ts
@@ -10,6 +10,7 @@ type ApiResponse = {
 
 export default function handler(_req: ApiRequest, res: ApiResponse) {
   res.setHeader("Cache-Control", "no-store");
+
   return res.status(200).json({
     ok: true,
     ts: new Date().toISOString(),
